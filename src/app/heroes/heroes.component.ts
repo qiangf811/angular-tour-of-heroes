fc4ff474c3aeec742f9cf41fb1ddb5aef783d976
fengqiang <qiangfeng@tunicorn.cn>
2019-03-08 18:07:27 +0800

    init

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService) {
+    if ({ a: '1' } === { a: '1' }) {
+      console.log(true)
+    }
+  }
+
+  heroes: Hero[];
+
+  selectedHero: Hero;
+
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes);
+  }
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
+}
